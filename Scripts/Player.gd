7d7851c81749de5278c40aaba947dca38d6e2c3d
FishingDonut <protronsdimi@gmail.com>
2024-03-09 19:19:38 -0300

    scriots

---
+++ Scripts/Player.gd
@@ -0,0 +1,56 @@
+extends CharacterBody2D
+
+
+const SPEED = 300.0
+const JUMP_VELOCITY = -400.0
+
+# Get the gravity from the project settings to be synced with RigidBody nodes.
+var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
+
+@onready var animation := $AnimatedSprite2D as AnimatedSprite2D
+var is_jump := false
+var is_puch := false
+
+func blink():
+	var tweenPlayer = get_tree().create_tween()
+	
+	if !is_on_floor():
+		tweenPlayer.tween_property(self, "modulate", Color(1, 1, 1), 0.15)
+	else:
+		tweenPlayer.tween_property(self, "modulate", Color(1,0, 0), 1).set_delay(1)
+		tweenPlayer.tween_property(self, "modulate", Color(0,0, 1), 1).set_delay(1)
+
+func _physics_process(delta):
+	#blink()
+	# Add the gravity.
+	if not is_on_floor():
+		velocity.y += gravity * delta
+		
+	# Handle jump.
+	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+		velocity.y = JUMP_VELOCITY
+		is_jump = true
+	elif is_on_floor():
+		is_jump = false
+
+	# Get the input direction and handle the movement/deceleration.
+	# As good practice, you should replace UI actions with custom gameplay actions.
+	var direction = Input.get_axis("ui_left", "ui_right")
+	if direction:
+		velocity.x = direction * SPEED
+		animation.scale.x = direction
+		
+		if is_on_floor() and !is_jump:
+			animation.play("run")
+		elif is_jump:
+			animation.play("jump")
+			
+	elif is_jump:
+		animation.play("jump")
+	elif is_on_floor() and Input.is_key_label_pressed(KEY_P):
+		animation.play("puch")
+	else:
+		velocity.x = move_toward(velocity.x, 0, SPEED)
+		animation.play("idle")
+
+	move_and_slide()
---
+++ Scripts/coin.gd
@@ -0,0 +1,34 @@
+extends Area2D
+
+@onready var animation := $coinAnimation as AnimatedSprite2D
+
+var follow := Vector2.ZERO
+var duration_idle := 0.6
+var distance_idle := 8
+var distance_collect := 20
+var duration_collect := 0.3
+
+# Called when the node enters the scene tree for the first time.
+
+func _ready():
+	move_coin()
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _physics_process(delta):
+	animation.position = follow
+
+
+func _on_body_shape_entered(body_rid, body, body_shape_index, local_shape_index):
+	var coinTween = create_tween().set_loops().set_trans(Tween.TRANS_QUAD)
+	coinTween.tween_property(self, "follow", Vector2.UP * distance_collect, duration_collect)
+	animation.play("collect")
+
+
+func _on_coin_animation_animation_finished():
+	queue_free()
+
+
+func move_coin():
+	var coinTween = create_tween().set_loops().set_trans(Tween.TRANS_QUAD)
+	coinTween.tween_property(self, "follow", Vector2.UP * distance_idle, duration_idle)
+	coinTween.tween_property(self, "follow", Vector2.ZERO, duration_idle)
