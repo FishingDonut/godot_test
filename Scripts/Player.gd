359823ac6c316c7c3b60dc04deff9393db5f9d1c
FishingDonut <protronsdimi@gmail.com>
2024-03-10 00:44:44 -0300

    update 02

--- Scripts/Player.gd
+++ Scripts/Player.gd
@@ -7,7 +7,7 @@ const JUMP_VELOCITY = -400.0
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 
-@onready var animation := $AnimatedSprite2D as AnimatedSprite2D
+@onready var animation := $PlayerAnimated as AnimatedSprite2D
 var is_jump := false
 var is_puch := false
 
---
+++ Scripts/enemy.gd
@@ -0,0 +1,38 @@
+extends CharacterBody2D
+
+const SPEED = 50.0
+const JUMP_VELOCITY = -200.0
+
+@onready var sprite := $enemySprite as Sprite2D
+@onready var animation := $AnimationEnemy as AnimationPlayer
+@onready var detectd_wall := $detectd_wall as RayCast2D
+
+# Get the gravity from the project settings to be synced with RigidBody nodes.
+var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
+var direction := 1
+
+func _physics_process(delta):
+	if not is_on_floor():
+		velocity.y += gravity * delta
+
+	#if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+		#velocity.y = JUMP_VELOCITY
+
+	if direction:
+		velocity.x = direction * SPEED
+		sprite.scale.x = direction
+		animation.play("run")
+	else:
+		velocity.x = move_toward(velocity.x, 0, SPEED)
+		animation.play("idle")
+	
+	if detectd_wall.is_colliding():
+		detectd_wall.scale.x *= -1
+		direction *= -1
+		if direction != 0:
+			sprite.flip_h = false
+		else:
+			sprite.flip_h = true
+		
+	
+	move_and_slide()
