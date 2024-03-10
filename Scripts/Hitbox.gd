53edc351ddf30a565703134c920701ee3d60d87a
FishingDonut <protronsdimi@gmail.com>
2024-03-10 12:17:16 -0300

    removed print

--- Scripts/Hitbox.gd
+++ Scripts/Hitbox.gd
@@ -2,8 +2,6 @@ extends Area2D
 
 
 func _on_body_entered(body):
-	print("owner ", owner.name)
-	print("body ", body.name)
 	if body.name == "Player":
 		body.velocity.y = body.JUMP_VELOCITY * 0.5
 		owner.animation.play("hurt")
